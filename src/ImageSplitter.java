d2948b8944154232482aae0b31e089bdd32504b4
Allan Demarbre <demarbre1u@portable-allan>
2017-03-03 11:20:18 +0100

    Modification de l'icône, correctiondes messages d'erreur (n'en affiche plus si le FileChoser est fermé sans avoir sélectionner de fichier), on demande maintenant le nombre de colonne / lignes et plus les dimensions de découpage, retrait de la ProgressBar de merde

--- src/ImageSplitter.java
+++ src/ImageSplitter.java
@@ -41,9 +41,12 @@ public class ImageSplitter extends JFrame {
 	 */
 	public static void main(String[] args) 
 	{
-		try {
+		try 
+		{
 			UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
-		} catch (Throwable e) {
+		} 
+		catch (Throwable e) 
+		{
 			e.printStackTrace();
 		}
 		EventQueue.invokeLater(new Runnable() 
@@ -73,19 +76,19 @@ public class ImageSplitter extends JFrame {
 		setTitle("Image Splitter");
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 376, 150);
+		setBounds(100, 100, 376, 135);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		
+
 		Panel panel = new Panel();
 		contentPane.add(panel, BorderLayout.NORTH);
 		panel.setLayout(new BorderLayout(0, 0));
-		
+
 		JLabel lblPath = new JLabel("Path : ");
 		panel.add(lblPath, BorderLayout.WEST);
-		
+
 		JButton btnBrowse = new JButton("Browse");
 		panel.add(btnBrowse, BorderLayout.EAST);
 		btnBrowse.addActionListener(new ActionListener() 
@@ -96,7 +99,7 @@ public class ImageSplitter extends JFrame {
 				JFileChooser fc = new JFileChooser();
 				fc.showOpenDialog(new JFrame("Chose an image"));
 				File file = fc.getSelectedFile();
-			
+
 				try 
 				{
 					if(ImageIO.read(file) != null)
@@ -104,57 +107,57 @@ public class ImageSplitter extends JFrame {
 						path.setText(fc.getSelectedFile().getAbsolutePath());
 					}
 					else 
-						throw new Exception("Not an image");
+						throw new FilePathException("Not an image");
 				} 
-				catch (Exception e1) 
+				catch (FilePathException e1) 
 				{
 					JOptionPane.showMessageDialog(null, "The selected file is not an image!", "Error", 0);
+				} 
+				catch (IOException e1) 
+				{
+					JOptionPane.showMessageDialog(null, "An error has occured while attempting to read the file!", "Error", 0);
 				}
+				catch(IllegalArgumentException iae) {}
 			}
 		});
-		
+
 		path = new JTextField();
 		panel.add(path, BorderLayout.CENTER);
 		path.setColumns(10);
-		
+
 		Panel panel_1 = new Panel();
 		contentPane.add(panel_1, BorderLayout.CENTER);
 		panel_1.setLayout(new BorderLayout(0, 0));
-		
+
 		Panel panel_2 = new Panel();
 		panel_1.add(panel_2, BorderLayout.NORTH);
-		
-		JLabel lblSizeX = new JLabel("Size X :");
+
+		JLabel lblSizeX = new JLabel("Column :");
 		panel_2.add(lblSizeX);
-		
+
 		// Creating a formatter so that those fields are always valid
 		NumberFormat format = NumberFormat.getInstance();
-	    NumberFormatter formatter = new NumberFormatter(format);
-	    formatter.setValueClass(Integer.class);
-	    formatter.setMinimum(1);
-	    formatter.setMaximum(Integer.MAX_VALUE);
-	    formatter.setAllowsInvalid(false);
-	    // If you want the value to be committed on each keystroke instead of focus lost
-	    formatter.setCommitsOnValidEdit(true);
-	    
+		NumberFormatter formatter = new NumberFormatter(format);
+		formatter.setValueClass(Integer.class);
+		formatter.setMinimum(1);
+		formatter.setMaximum(Integer.MAX_VALUE);
+		formatter.setAllowsInvalid(false);
+		// If you want the value to be committed on each keystroke instead of focus lost
+		formatter.setCommitsOnValidEdit(true);
+
 		sizeX = new JFormattedTextField(formatter);
 		sizeX.setText("1");
 		panel_2.add(sizeX);
 		sizeX.setColumns(10);
-		
-		JLabel lblSizeY = new JLabel("Size Y :");
+
+		JLabel lblSizeY = new JLabel("Row :");
 		panel_2.add(lblSizeY);
-		
+
 		sizeY = new JFormattedTextField(formatter);
 		sizeY.setText("1");
 		panel_2.add(sizeY);
 		sizeY.setColumns(10);
-		
-		JProgressBar bar = new JProgressBar(0, 100);
-		bar.setValue(0);
-		bar.setStringPainted(true);
-		panel_1.add(bar, BorderLayout.SOUTH);
-		
+
 		JButton btnProcess = new JButton("Process");
 		panel_1.add(btnProcess, BorderLayout.CENTER);
 		btnProcess.addActionListener(new ActionListener()
@@ -166,47 +169,52 @@ public class ImageSplitter extends JFrame {
 				{
 					if(path.getText().isEmpty())
 						throw new FilePathException("not file path specified");
-					
+
 					if(sizeX.getText().isEmpty() || sizeY.getText().isEmpty())
 						throw new SizeFieldException("No size values");
 
 					// Creating an image
 					BufferedImage img = null;
 					img = ImageIO.read(new File(path.getText()));
-					
+
 					// Creating folder where to create sub-images
 					File fileName = new File(path.getText());
 					File folder = new File("img/" + fileName.getName());
 					folder.mkdir();
-					
+
 					// Creating all the sub-images
-					int width = img.getWidth() / Integer.parseInt(sizeX.getText());
-					int height = img.getHeight() / Integer.parseInt(sizeY.getText());
+					int column = Integer.parseInt(sizeX.getText());
+					int row = Integer.parseInt(sizeY.getText());
+					int width = img.getWidth() / column;
+					int height = img.getHeight() / row;
 					int subCount = 1;
-					
+
 					//bar.setMaximum(width*height);
-					
-					for(int j = 0 ; j < height ; j++)
+
+					for(int j = 0 ; j < row ; j++)
 					{
-						for(int i = 0 ; i < width ; i++)
+						for(int i = 0 ; i < column ; i++)
 						{	
-							File outputfile = new File("img/" + folder.getName() + "/sub" + subCount + ".png");
+							int size1 = ("" + (row * column)).length();
+							int size2 = ("" + subCount).length();
+							String placeholder = "";
+							for(int h = 0 ; h < (size1 - size2) ; h++)
+							{
+								placeholder += "0";
+							}
+
+							File outputfile = new File("img/" + folder.getName() + "/sub" + placeholder + subCount + ".png");
 							outputfile.createNewFile();
-							BufferedImage sub = img.getSubimage(i * Integer.parseInt(sizeX.getText()), 
-																j * Integer.parseInt(sizeY.getText()), 
-																Integer.parseInt(sizeX.getText()), 
-																Integer.parseInt(sizeY.getText()));
+							BufferedImage sub = img.getSubimage(i * width, 
+									j * height, 
+									width,
+									height);
 							ImageIO.write(sub, "png", outputfile);
 							subCount++;
 						}
-						
-						
-						float per = ((float) subCount / (width*height) * 100);
-						bar.setValue((int) per);
 					}
-					
+
 					JOptionPane.showMessageDialog(null, "The image has been successfuly splitted!", "Info", 1);
-					bar.setValue(0);
 				}
 				catch(SizeFieldException sfe)
 				{
